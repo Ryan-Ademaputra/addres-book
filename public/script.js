743e308e466d66a380e7c9c881265d875b1f3b98
Ryan-Ademaputra <ademaputraryan@gmail.com>
2024-08-03 14:13:11 +0700

    fix code js

--- public/script.js
+++ public/script.js
@@ -3,31 +3,34 @@ const bookContacts = [
         id: 1,
         fullName: "Ryan Ademaputra",
         email: "ademaputraryan@gmail.com",
-        phoneNumber: +6285860987806,
+        phoneNumber: "+6285860987806",
         label: "friend",
         birthday: "14 - 03 - 2006",
         avatar: "",
+        addres:""
     },
 
     {
         id: 2,
         fullName: "Dilan",
-        email: "ademaputradilan@gmail.com",
-        phoneNumber: +6285860987806,
+        email: "putradilan@gmail.com",
+        phoneNumber: "+6285860987806",
         label: "work",
         birthday: "15 - 09 - 2001",
         avatar: "",
+        addres:""
 
     },
 
     {
         id: 2,
-        fullName: "Dilan",
-        email: "ademaputradilan@gmail.com",
-        phoneNumber: +6285860987806,
+        fullName: "roma",
+        email: "ademaputra@gmail.com",
+        phoneNumber: "+6285860987806",
         label: "work",
         birthday: "15 - 09 - 2001",
         avatar: "",
+        addres:""
 
     }
 ];
