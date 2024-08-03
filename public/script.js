cd36f1dd9f1404909a9515db4a7f9f7f576119f5
M Haidar Hanif <git@mhaidarhanif.com>
2024-08-03 14:51:54 +0700

    refine data contacts

--- public/script.js
+++ public/script.js
@@ -1,38 +1,44 @@
-const bookContacts = [
-    {
-        id: 1,
-        fullName: "Ryan Ademaputra",
-        email: "ademaputraryan@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "friend",
-        birthday: "14 - 03 - 2006",
-        avatar: "",
-        addres:""
-    },
+// Address Book is the name of the app
+// Contacts is the name of the data
+// Employees is the name of the data
 
-    {
-        id: 2,
-        fullName: "Dilan",
-        email: "putradilan@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "work",
-        birthday: "15 - 09 - 2001",
-        avatar: "",
-        addres:""
+// Facebook is the app. Friends is the data
+// Instagram is the app. Followers, Followings is the data
 
-    },
+const dataContacts = [
+  {
+    id: 1,
+    fullName: "Ryan Ademaputra",
+    email: "ademaputraryan@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "friend",
+    birthday: "14-03-2006",
+    avatarURL: "/assets/ryan.jpeg",
+    // avatarURL: "https://api.dicebear.com/9.x/thumbs/svg?seed=Ryan",
+    address: "",
+  },
 
-    {
-        id: 2,
-        fullName: "roma",
-        email: "ademaputra@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "work",
-        birthday: "15 - 09 - 2001",
-        avatar: "",
-        addres:""
+  {
+    id: 2,
+    fullName: "Dilan",
+    email: "putradilan@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "work",
+    birthday: "15-09-2001",
+    avatarURL: "",
+    address: "",
+  },
 
-    }
+  {
+    id: 2,
+    fullName: "roma",
+    email: "ademaputra@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "work",
+    birthday: "15-09-2001",
+    avatarURL: "",
+    address: "",
+  },
 ];
 
-console.log(bookContacts)
+console.log(dataContacts);
