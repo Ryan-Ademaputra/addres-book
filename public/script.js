02adf12ac1f5d4031b88816970a8609b53ff886b
Ryan-Ademaputra <ademaputraryan@gmail.com>
2024-08-10 22:04:55 +0700

    feat : format data

--- public/script.js
+++ public/script.js
@@ -1,38 +1,70 @@
-const bookContacts = [
-    {
-        id: 1,
-        fullName: "Ryan Ademaputra",
-        email: "ademaputraryan@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "friend",
-        birthday: "14 - 03 - 2006",
-        avatar: "",
-        addres:""
-    },
-
-    {
-        id: 2,
-        fullName: "Dilan",
-        email: "putradilan@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "work",
-        birthday: "15 - 09 - 2001",
-        avatar: "",
-        addres:""
-
-    },
-
-    {
-        id: 2,
-        fullName: "roma",
-        email: "ademaputra@gmail.com",
-        phoneNumber: "+6285860987806",
-        label: "work",
-        birthday: "15 - 09 - 2001",
-        avatar: "",
-        addres:""
-
-    }
+const dataContacts = [
+  {
+    id: 1, // ID unik
+    fullName: "Ryan Ademaputra",
+    email: "ademaputraryan@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "friend",
+    birthday: "2006-03-14",
+    avatarURL: "/assets/ryan.jpeg",
+    address: "",
+  },
+  {
+    id: 2,
+    fullName: "Ademaputra",
+    email: "ryan@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "work",
+    birthday: "2005-03-15",
+    avatarURL: "/assets/ryan.jpeg",
+    address: "",
+  },
+  {
+    id: 3,
+    fullName: "Ryan Ademaputra",
+    email: "ademaputraryan@gmail.com",
+    phoneNumber: "+6285860987806",
+    label: "friend",
+    birthday: "2001-02-20",
+    avatarURL: "/assets/ryan.jpeg",
+    address: "",
+  },
 ];
 
-console.log(bookContacts)
+function addContact(dataContacts, newContact) {
+  dataContacts.push(newContact);
+
+  console.log(dataContacts);
+}
+
+//function format data contact
+function formatDataContacts(dataContact) {
+  return `${dataContact.id} ${dataContact.fullName} ${dataContact.email} ${dataContact.phoneNumber} ${dataContact.label} ${dataContact.birthday} ${dataContact.avatarURL} ${dataContact.address}`;
+}
+
+//function display data contact console
+function displayContacts(dataContacts) {
+  dataContacts.forEach((dataContact) => {
+    console.log(formatDataContacts(dataContact));
+  });
+}
+addContact(dataContacts, {
+  id: 3,
+  fullName: "Ryan Ademaputra",
+  email: "ademaputraryan@gmail.com",
+  phoneNumber: "+6285860987806",
+  label: "friend",
+  birthday: "2001-02-20",
+  avatarURL: "/assets/ryan.jpeg",
+  address: "",
+});
+
+// display data contact
+displayContacts(dataContacts);
+
+localStorage.setItem("data", [
+  {
+    nama: "Ryan Ademaputra",
+    umur: "18",
+  },
+]);
